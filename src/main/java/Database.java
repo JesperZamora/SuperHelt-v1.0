29d42b6863c92f3d11ba85b8906577d81afb5c4b
JesperZamora <jesp78m6@stud.kea.dk>
2022-09-14 19:06:17 +0200

    Nyt søgepunkt i menu "søg efter superhelt". ny metode i database for at finde superheltenavn.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Database {
     private ArrayList<Superhelt> superhelteData = new ArrayList();
-    
+
     public void tilføjSuperhelt(String aliasNavn, String superNavn, int oprindelsesÅr, boolean menneske, String superkraft, double styrke) {
 
         Superhelt superhelt = new Superhelt(aliasNavn, superNavn, oprindelsesÅr, menneske, superkraft, styrke);
@@ -13,5 +13,29 @@ public class Database {
         return superhelteData;
     }
 
+   boolean fundet = true;
+    public Superhelt findSuperhelt (String navn) {
+        for (int i = 0; i <superhelteData.size(); i++) {
+            if(superhelteData.get(i).getSuperNavn().equals(navn)) {
+                return superhelteData.get(i);
+
+            } else if (superhelteData.get(i).getSuperNavn().contains(navn)) {
+                return superhelteData.get(i);
+            }
+        }
+        return null;
+    }
+
+    /*
+    public Superhelt findSuperhelt (String navn) {
+        for (Superhelt superhelteDatum : superhelteData) {
+            if (superhelteDatum.getSuperNavn().equals(navn)) {
+                return superhelteDatum;
+            }
+        }
+        return null;
+    }
+
+     */
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         Database superData = new Database();
         int brugerValg = 0;
 
         do {
             System.out.println("""
                 \nVelkommen til Superhelte-programmet
-                Tast 1. for at starte programmet
-                Tast 2. for at udskrive alle helte
+                Tast 1. Start programmet
+                Tast 2. Søg efter superhelt
+                Tast 3. Udskiv alle superhelte
                 Tast 9. for at slutte programmet
                 """);
 
@@ -43,12 +43,21 @@ public class Main {
                 int oprindelsesÅr = sc.nextInt();
                 sc.nextLine();
 
-                System.out.print("Indtast superheltens styrkeniveau fra 1-10000: ");
+                System.out.print("Indtast superheltens styrkeniveau fra 1-1000000: ");
                 double styrke = sc.nextDouble();
 
                 superData.tilføjSuperhelt(aliasNavn, superNavn, oprindelsesÅr, menneske, superkraft, styrke);
 
-            } else if(brugerValg == 2) {
+
+            } else if (brugerValg == 2) {
+                System.out.println("Find superhelt. Indtast superheltens navn: ");
+                String findHelt = sc.next();
+                Superhelt superhelt = superData.findSuperhelt(findHelt);
+                if (superhelt == null)
+                    System.out.println("Superhelt " + findHelt+ " ikke fundet");
+                else System.out.println(superhelt);
+
+            } else if(brugerValg == 3) {
                 System.out.println("\nSuperheltens stats");
 
                 for (Superhelt superhelt : superData.getSuperhelteData()) {
